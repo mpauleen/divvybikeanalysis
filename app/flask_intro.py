cccebcf0a0f4277ae75495a43be0dcdac69a8f82
mpauleen <michaelpauleen2018@u.northwestern.edu>
2018-02-07 12:00:12 -0600

    Flask plugs into model
    - Clean up old code
    - Dynamic front end based on model return

--- app/flask_intro.py
+++ app/flask_intro.py
@@ -1,4 +1,8 @@
 from flask import Flask, render_template, request
+import sys
+import os
+sys.path.append(os.path.abspath('../develop/models'))
+from predict import predict_shortage
 
 app = Flask(__name__)
 
@@ -12,28 +16,11 @@ def index():
 def predict():
 #    station_id = request.form['id']
     vars = request.args.to_dict()
+    prediction = round(predict_shortage(vars['station']),2)*100
     print vars
 #    print station_id, map_long, map_lat, map_zoom
-    return render_template("index.html", has_result = True, station_id = vars['station'], map_lat = vars['lat'], map_long = vars['lng'], map_zoom = vars['zoom'])
+    return render_template("index.html", has_result = True, station_id = vars['station'], map_lat = vars['lat'], map_long = vars['lng'], map_zoom = vars['zoom'], prediction = prediction)
 #, map_long = map_long, map_lat = map_lat, map_zoom = map_zoom
-@app.route('/test')
-def test():
-    return '<h2>Testing</h2>' #you can write html in here!
-
-#now username is a variable if inside "<>"
-@app.route('/profile/<username>')
-def profile(username):
-   return '<h2>Hey there %s<h2>' % username
-
-#for integers you need to specify type int
-@app.route('/post/<int:post_id>')
-def post(post_id):
-   return '<h2>Post ID is %s<h2>' % post_id
-
-#Using HTML Templates
-@app.route("/profile2/<name>")
-def profile2(name):
-    return render_template("profile.html",name=name)
 
 if __name__ == "__main__":
     app.run(debug=True)
